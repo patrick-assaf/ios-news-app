1163cfbe3bc5b21bc4f73aafe48dd8a0f3b0ae1e
Patrick Assaf <patrickassaf96@gmail.com>
2020-04-20 16:31:22 -0400

    Create additional view controllers for trending and bookmarks tabs

---
+++ ios-news-app/ios-news-app/TrendingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TrendingViewController.swift
+//  ios-news-app
+//
+//  Created by Patrick Assaf on 4/20/20.
+//  Copyright © 2020 patrickassaf. All rights reserved.
+//
+
+import UIKit
+
+class TrendingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
