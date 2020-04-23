761528d317da4ceee96d1316f04582d94971a8a9
Patrick Assaf <patrickassaf96@gmail.com>
2020-04-22 23:43:07 -0400

    Define URL for OpenWeatherAPI call

--- ios-news-app/ios-news-app/HomeViewController.swift
+++ ios-news-app/ios-news-app/HomeViewController.swift
@@ -29,6 +29,7 @@ class HomeViewController: UIViewController, CLLocationManagerDelegate {
                     self.cityLabel.text = "\(city)"
                     self.stateLabel.text = "\(state)"
                 }
+                let weatherURL = URL(string: "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid=9031b6d8f8514c01eeaaf398a4188f8b")
             }
         })
     }
