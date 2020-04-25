663d70f37926d227369314959ea2e02eeb64b627
Patrick Assaf <patrickassaf96@gmail.com>
2020-04-25 15:27:49 -0400

    Connect outlet to table cell view for styling

--- ios-news-app/ios-news-app/HomeArticlesTableViewCell.swift
+++ ios-news-app/ios-news-app/HomeArticlesTableViewCell.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class HomeArticlesTableViewCell: UITableViewCell {
     
+    @IBOutlet weak var articleView: UIView! {
+        didSet {
+            articleView.layer.cornerRadius = 10
+            articleView.layer.shadowOpacity = 1
+            articleView.layer.shadowRadius = 2
+            articleView.layer.shadowColor = UIColor(named: "Orange")?.cgColor
+            articleView.layer.shadowOffset = CGSize(width: 3, height: 3)
+            articleView.backgroundColor = UIColor(named: "Red")
+        }
+    }
+    
+    @IBOutlet weak var articleContainer: UIView!
+    
     @IBOutlet weak var articleImage: UIImageView!
     @IBOutlet weak var articleTitle: UILabel!
     @IBOutlet weak var articleDate: UILabel!
