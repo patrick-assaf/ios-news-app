c7b3c458e85ba51be2914b23dd1a7d03814ba962
Patrick Assaf <patrickassaf96@gmail.com>
2020-05-05 11:07:39 -0400

    Hide navigation bar from home view controller

--- ios-news-app/ios-news-app/HomeViewController.swift
+++ ios-news-app/ios-news-app/HomeViewController.swift
@@ -153,6 +153,14 @@ class HomeViewController: UIViewController, CLLocationManagerDelegate, UISearchB
         }
         
         super.viewWillAppear(animated)
+        
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        navigationController?.setNavigationBarHidden(false, animated: false)
     }
     
     @objc func refresh(_ sender: AnyObject) {
