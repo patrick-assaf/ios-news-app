b46d6aeb49a1b1cf72f874644ecda1e2d74ac104
Patrick Assaf <patrickassaf96@gmail.com>
2020-05-05 22:12:36 -0400

    Implement multipage view displaying sections in headlines tab

--- ios-news-app/ios-news-app/HeadlinesViewController.swift
+++ ios-news-app/ios-news-app/HeadlinesViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import SwiftSpinner
+import XLPagerTabStrip
 
-class HeadlinesViewController: UIViewController {
+class HeadlinesViewController: ButtonBarPagerTabStripViewController {
 
     override func viewDidLoad() {
+        
+        settings.style.buttonBarBackgroundColor = .white
+        settings.style.buttonBarItemBackgroundColor = .white
+        settings.style.buttonBarItemFont = .boldSystemFont(ofSize: 14)
+        settings.style.selectedBarHeight = 3.0
+        settings.style.buttonBarMinimumLineSpacing = 0
+        settings.style.buttonBarItemTitleColor = .black
+        settings.style.buttonBarItemsShouldFillAvailableWidth = true
+        settings.style.buttonBarLeftContentInset = 0
+        settings.style.buttonBarRightContentInset = 0
+        
+        changeCurrentIndexProgressive = { (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
+            guard changeCurrentIndex == true else { return }
+            oldCell?.label.textColor = .black
+            newCell?.label.textColor = .blue
+        }
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+        let world = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "World")
+        let business = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Business")
+        return [world, business]
+    }
 
 }
 
---
+++ ios-news-app/ios-news-app/World.swift
@@ -0,0 +1,44 @@
+//
+//  World.swift
+//  ios-news-app
+//
+//  Created by Patrick Assaf on 5/5/20.
+//  Copyright © 2020 patrickassaf. All rights reserved.
+//
+
+import UIKit
+import XLPagerTabStrip
+
+class World: UITableViewController, IndicatorInfoProvider {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let articleCell = tableView.dequeueReusableCell(withIdentifier: "articleCell", for: indexPath) as! HomeArticlesTableViewCell
+        
+        return articleCell
+    }
+    
+    func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+        return IndicatorInfo(title: "World")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
