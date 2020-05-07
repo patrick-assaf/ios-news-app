2a8bc1e9b45fb74606e042fa22f41b03c1a69d0b
Patrick Assaf <patrickassaf96@gmail.com>
2020-05-07 11:11:04 -0400

    Implement line chart view in trending tab

--- ios-news-app/ios-news-app/TrendingViewController.swift
+++ ios-news-app/ios-news-app/TrendingViewController.swift
@@ -7,13 +7,58 @@
 //
 
 import UIKit
+import Charts
 
-class TrendingViewController: UIViewController {
+class TrendingViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var textBox: UITextField!
+    @IBOutlet weak var chartView: LineChartView!
+    
+    var numbers : [Double] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.textBox.delegate = self
+        
     }
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.text = ""
+    }
+    
+    @IBAction func textFieldAction(_ sender: Any) {
+        self.view.endEditing(true)
+        let input = Double(textBox.text!)
+        numbers.append(input!)
+        if(textBox.text != "") {
+            updateChart()
+        }
+    }
+    
+    func updateChart() {
+        
+        var lineChartPoints  = [ChartDataEntry]()
+        
+        for i in 0..<numbers.count {
+            let value = ChartDataEntry(x: Double(i), y: numbers[i])
+            lineChartPoints.append(value)
+        }
+        
+        let line = LineChartDataSet(entries: lineChartPoints, label: "Number")
+        line.colors = [UIColor.systemBlue]
+        line.circleColors = [UIColor.systemBlue]
+        line.circleHoleColor = UIColor.systemBlue
+        
+        let data = LineChartData()
+        data.addDataSet(line)
+        
+        chartView.data = data
+    }
+    
 
 }
